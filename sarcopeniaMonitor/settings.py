0814ee03c4bf17f5447e0e6a19583e47e2f54893
christmaskid <chengyt2001@gmail.com>
2025-03-16 02:06:40 +0800

    Add static

--- sarcopeniaMonitor/settings.py
+++ sarcopeniaMonitor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#ynonhe!p3py32w_z_2hm-qk!my*bmcwb-%mbmqe=upwr&vmj@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False #True
+DEBUG = True # False #
 
 ALLOWED_HOSTS = [
     '*', 
@@ -142,6 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
