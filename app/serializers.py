ec06a26af6f3930020be10e653a651c0d0c1a8f9
christmaskid <chengyt2001@gmail.com>
2025-03-23 07:05:06 +0800

    Update serializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import UserRecord, RECORD_FIELDS
+from .models import UserRecord, RECORD_FIELDS, Questionnaire
 
 class UserRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserRecord
+        fields = '__all__'  # Include all fields
+
+class QuesionnaireSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Questionnaire
         fields = '__all__'  # Include all fields
